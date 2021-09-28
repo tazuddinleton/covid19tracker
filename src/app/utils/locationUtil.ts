277fa1da88cc92305b68d3e8647f7eca0deb38a2
Taz Uddin <getleton@gmail.com>
2021-09-29 03:59:24 +0600

    Refactor

--- src/app/utils/locationUtil.ts
+++ src/app/utils/locationUtil.ts
@@ -2,17 +2,17 @@ import { Continent, Country, RawLocation } from "../models/location/location";
 
 export class LocUtils{
   public static asCountry(rl: RawLocation): Country{
-    return {
+    return <Country>{
       code: rl.Two_Letter_Country_Code,
       name: rl.Country_Name
-    } as Country;
+    };
   }
 
   public static asContinent(rl: RawLocation): Country{
-    return {
+    return <Continent>{
       code: rl.Continent_Code,
       name: rl.Continent_Name,
       countries: [LocUtils.asCountry(rl)]
-    } as Continent;
+    };
   }
 }
